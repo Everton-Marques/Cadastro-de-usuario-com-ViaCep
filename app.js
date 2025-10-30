a41f9919e708eba9e6b636527db5fbddf5ed06db
Everton-Marques <Marquesevertong@gmail.com>
2025-10-30 18:52:51 -0400

    javascript finalizado, correções no html

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const cep = document.getElementById("cepIn");
 const city = document.getElementById("cityIn");
 const estado = document.getElementById("estadoIn");
-cep.addEventListener("blur",(e)=>{
-    e.preventDefault();
-     const valorCep = cep.value.replace(/\D/g, ''); // remove tudo que não é número
+cep.addEventListener("blur", (e) => {
+    const valorCep = cep.value.replace(/\D/g, '');
     const validacep = /^[0-9]{8}$/;
 
     if (!validacep.test(valorCep)) {
@@ -11,5 +10,30 @@ cep.addEventListener("blur",(e)=>{
     } else {
         city.value = "Carregando...";
         estado.value = "Carregando...";
+        let cepsaida = valorCep;
+        alert(cepsaida)
+        fetch(`https://viacep.com.br/ws/${valorCep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                city.value = data.localidade;
+                estado.value = data.uf;
+            })
     }
 })
+
+const refForm = document.querySelector("form")
+function registroStorage() {
+    const formData = new FormData(refForm);
+    const dados = Object.fromEntries(formData.entries());
+    localStorage.setItem("cadastro", JSON.stringify(dados));
+    alert("Dados salvos com sucesso!");
+}
+window.addEventListener("load", () => {
+    const dadosSalvos = JSON.parse(localStorage.getItem("cadastro"));
+    if (dadosSalvos) {
+        Object.entries(dadosSalvos).forEach(([id, valor]) => {
+            const campo = document.getElementById(id);
+            if (campo) campo.value = valor;
+        });
+    }
+});
